b0c641f8394615a4a18d553aff21fe3ef19ea626
oussemabenhassena5 (github) <oussemabenhassena5@gmail.com>
2025-01-27 16:12:48 +0100

    Add: upload_data route

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,9 +1,78 @@
-from fastapi import APIRouter, UploadFile,Depends
-from helpers.config import Settings, get_settings
+import logging
+
+from fastapi import APIRouter, Depends, Request, UploadFile, status
+from fastapi.responses import JSONResponse
+
+from models import ResponseSignal
+from models.AssetModel import AssetModel
+from models.db_schemes import Asset
+from models.enums.AssetTypeEnum import AssetTypeEnum
+from models.ProjectModel import ProjectModel
 
 
+logger = logging.getLogger("uvicorn.error")
 
-data_router = APIRouter()
+data_router = APIRouter(
+    prefix="/api/v1/data",
+    tags=["api_v1", "data"],
+)
 
-@data_router.get("/upload/{project_id}")
-async def upload_data(project_id: str,file: UploadFile,app_settings: Settings = Depends(get_settings)):
+
+@data_router.post("/upload/{project_id}")
+async def upload_data(
+    request: Request,
+    project_id: str,
+    file: UploadFile,
+    app_settings: Settings = Depends(get_settings),
+):
+
+    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
+
+    project = await project_model.get_project_or_create_one(project_id=project_id)
+
+    # validate the file properties
+    data_controller = DataController()
+
+    is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
+
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST, content={"signal": result_signal}
+        )
+
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    file_path, file_id = data_controller.generate_unique_filepath(
+        orig_file_name=file.filename, project_id=project_id
+    )
+
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+
+        logger.error(f"Error while uploading file: {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"signal": ResponseSignal.FILE_UPLOAD_FAILED.value},
+        )
+
+    # store the assets into the database
+    asset_model = await AssetModel.create_instance(db_client=request.app.db_client)
+
+    asset_resource = Asset(
+        asset_project_id=project.id,
+        asset_type=AssetTypeEnum.FILE.value,
+        asset_name=file_id,
+        asset_size=os.path.getsize(file_path),
+    )
+
+    asset_record = await asset_model.create_asset(asset=asset_resource)
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+            "file_id": str(asset_record.id),
+        }
+    )
